dbbb0f391d60e8c4bd048b7fdd21c73a769e1b8e
jakkaphan_5741 <jakkaphan_5741@hotmail.com>
2023-11-23 12:00:26 +0700

    Updates

--- src/confix/project.config.ts
+++
@@ -1,5 +0,0 @@
-export const projectConfig = {
-  BASE_URL: import.meta.env.REACT_APP_API_SERVICE || "https://pokeapi.co",
-  API_VERSION: import.meta.env.REACT_APP_API_VERSION || "/api/v2",
-};
-export default projectConfig;
